656e5720d48fce7f28712909907c24c52ca0aaf6
Sarah <sara.emilia.andersson@gmail.com>
2023-12-03 20:20:50 +0100

    add secret santa program

---
+++ Python/src/Secret_Santa/secret_santa.py
@@ -0,0 +1,26 @@
+import json
+import random
+
+
+def read_file(file_name) -> dict[str, str]:
+    with open(file_name) as f:
+        return json.load(f)
+
+
+def generate_pairs(persons: list[str]):
+    persons_shuffled = persons.copy()
+    random.shuffle(persons_shuffled)
+    return {person: persons_shuffled[(idx + 1) % len(persons_shuffled)] for idx, person in enumerate(persons_shuffled)}
+
+
+def write_to_file(file, pairings):
+    with open(file, "w") as f:
+        json.dump(pairings, f)
+
+
+def main(file_name):
+    persons = read_file("folks.txt")
+    pairings = generate_pairs(list(persons))
+    write_to_file(file_name, pairings)
+
+
