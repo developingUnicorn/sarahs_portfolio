14db38734b37d9992793e3e59fe24463fcbef9cd
Sarah <sara.emilia.andersson@gmail.com>
2024-02-16 13:49:52 +0100

    remove django intro

--- JavaProjects/StoreHandler/src/Item.java
+++ JavaProjects/StoreHandler/src/Item.java
@@ -1,7 +1,4 @@
-// PROG2 VT2021, Inlämningsuppgift, del 1
-// Grupp 056
-// Sarah Andersson saan3355
-// Malin Söderlund maso1928
+
 
 public abstract class Item implements Vat {
     protected final String name;
--- JavaProjects/StoreHandler/src/Vat25.java
+++ JavaProjects/StoreHandler/src/Vat25.java
@@ -1,7 +1,3 @@
-// PROG2 VT2021, Inlämningsuppgift, del 1
-// Grupp 056
-// Sarah Andersson saan3355
-// Malin Söderlund maso1928
 
 public interface Vat25 extends Vat{
 
